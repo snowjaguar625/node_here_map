5bdc22f2780875e75ce8b2845544cb70c79edd4d
Shweta <shweta.swarnima@gmail.com>
2021-06-01 23:34:39 +0530

    Clean referenc of default_map.json

--- routes/examples/v31/simple_map.js
+++ routes/examples/v31/simple_map.js
@@ -4,12 +4,9 @@ const path = require('path');
 var express = require('express');
 var router = express.Router();
 
-let rawMapDefault = fs.readFileSync(path.resolve(__dirname, '../../../config/map_default.json'));
-let mapDefault = JSON.parse(rawMapDefault);
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('examples/v31/simple_map', { title: 'Simple HERE Map Display', center:  mapDefault.center, zoom: mapDefault.zoom });
+  res.render('examples/v31/simple_map', { title: 'Simple HERE Map Display' });
 });
 
 module.exports = router;
